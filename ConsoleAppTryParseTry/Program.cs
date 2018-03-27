62ed3eb21ac78e4fb86ec5b95ca4e57f7220d9cd
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-03-27 13:32:20 +0200

    Program

--- ConsoleAppTryParseTry/Program.cs
+++ ConsoleAppTryParseTry/Program.cs
@@ -9,6 +9,25 @@ namespace ConsoleAppTryParseTry
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welk getal?");
+            string invoer = Console.ReadLine();
+        
+            int getal;
+            bool succes = false;
+            while (!succes)
+            {
+                if()
+                ChangeValue(invoer, out getal);
+                Console.WriteLine("Getal {0} heeft nu een int value", getal);
+
+            }
+          
+
+        }
+
+        static void ChangeValue(string invoer, out int getal)
+        {
+            Int32.TryParse(invoer, out getal);
         }
     }
 }
